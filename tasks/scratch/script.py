a9d3d655af5e5c56d48025cb488e523cbe233590
Nissim Lebovits <111617674+nlebovits@users.noreply.github.com>
2024-04-03 14:33:42 -0400

    reorg files; get opa_properties tasks running

---
+++ tasks/scratch/script.py
@@ -0,0 +1,9 @@
+import pretty_errors
+
+from utils.extract_data.extract_opa_properties import extract_phl_opa_properties
+from utils.prepare_data.prepare_opa_properties import prepare_phl_opa_properties
+from utils.run_sql.load_opa_properties import load_phl_opa_properties
+
+# extract_phl_opa_properties()
+# prepare_phl_opa_properties()
+load_phl_opa_properties()
