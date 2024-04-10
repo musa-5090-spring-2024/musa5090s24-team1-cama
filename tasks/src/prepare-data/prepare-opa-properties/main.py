0fe84574b84167cb0b49391199f04f2cbd09bbdc
brookeva <brookeva@upenn.edu>
2024-04-10 13:02:25 -0400

    Update main.py

--- tasks/src/prepare-data/prepare-opa-properties/main.py
+++ tasks/src/prepare-data/prepare-opa-properties/main.py
@@ -1,13 +1,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-import csv
 import json
 import os
 import pathlib
 
+import logging
 import pyproj
-from shapely import wkt
+from shapely.geometry import shape, mapping
+from shapely.ops import transform
 import functions_framework
 from google.cloud import storage
 
@@ -17,48 +18,45 @@ DIRNAME = pathlib.Path(__file__).parent
 def prepare_phl_opa_properties(request):
     print('Preparing OPA Properties data...')
 
-    raw_filename = DIRNAME / 'phl_opa_properties.csv'
+    raw_filename = DIRNAME / 'opa_properties_public.geojson'
     prepared_filename = DIRNAME / 'phl_opa_properties.jsonl'
-
-    # Input bucket
-    bucket_name = os.getenv('INPUT_DATA_LAKE_BUCKET')
-    storage_client = storage.Client()
-    input_bucket = storage_client.bucket(bucket_name)
-
-    # Output bucket
-    output_bucket_name = os.getenv('OUTPUT_DATA_LAKE_BUCKET')  # New environment variable for output bucket
-    output_bucket = storage_client.bucket(output_bucket_name)
 
     # Download the data from the input bucket
     raw_blobname = 'raw/phl_opa_properties/phl_opa_properties.csv'
+    prepared_blobname = 'tables/phl_opa_properties/phl_opa_properties.jsonl'
+    
+    bucket_name = os.getenv('INPUT_DATA_LAKE_BUCKET')
+    output_bucket_name = os.getenv('OUTPUT_DATA_LAKE_BUCKET')
+    storage_client = storage.Client()
+    input_bucket = storage_client.bucket(bucket_name)
+    output_bucket = storage_client.bucket(output_bucket_name)
+
     blob = input_bucket.blob(raw_blobname)
     blob.download_to_filename(raw_filename)
     print(f'Downloaded to {raw_filename}')
 
     # Load the data from the CSV file
     with open(raw_filename, 'r', encoding='utf-8') as f:
-        reader = csv.DictReader(f)
-        data = list(reader)
+        geojson_data = json.load(f)
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     # Set up the projection
-    transformer = pyproj.Transformer.from_proj('epsg:2272', 'epsg:4326')
+    transformer = pyproj.Transformer.from_proj('epsg:2272', 'epsg:4326', always_xy=True)
 
-    # Write the data to a JSONL file
-    with open(prepared_filename, 'w') as f:
-        for i, row in enumerate(data):
-            geom_wkt = row.pop('shape').split(';')[1]
-            if geom_wkt == 'POINT EMPTY':
-                row['geog'] = None
-            else:
-                geom = wkt.loads(geom_wkt)
-                x, y = transformer.transform(geom.x, geom.y)
-                row['geog'] = f'POINT({x} {y})'
-            f.write(json.dumps(row) + '\n')
+    def process_data(geojson_data, prepared_filename):
+        # Open the output file for writing
+        with open(prepared_filename, 'w') as f:
+            for feature in geojson_data['features']:
+                geom = shape(feature['geometry'])
+                # Apply transformation directly to the Shapely geometry
+                transformed_geom = transform(transformer.transform, geom)
+                feature['geometry'] = mapping(transformed_geom)
+                # Write each transformed feature as a separate line in JSONL format
+                f.write(json.dumps(feature) + '\n')
 
-    print(f'Processed data into {prepared_filename}')
+     logging.info(f'Processed data into {prepared_filename}')
 
-    # Upload the prepared data to the output bucket
-    prepared_blobname = 'tables/phl_opa_properties/phl_opa_properties.jsonl'
     blob = output_bucket.blob(prepared_blobname)
     blob.upload_from_filename(prepared_filename)
     print(f'Uploaded to {prepared_blobname} in {output_bucket_name}')
